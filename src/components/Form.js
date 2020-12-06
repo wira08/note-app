fd087cf275c6abdd8dd255edf9dfa6d0a1f80814
wira <wira08@live.se>
2020-12-06 17:55:20 +0100

    remove commented codes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,7 @@
-// import React from 'react'
+import React from 'react'
 import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
-// import {createNote} from '../utils/noteHelpers'
 
 export default function Form() {
   return (
--- src/components/NewNoteBtn.js
+++ src/components/NewNoteBtn.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
-// import {createNote} from '../utils/noteHelpers'
 
 export default function NewNoteBtn() {
   return (
